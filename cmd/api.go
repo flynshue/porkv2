be6a4be2425f4e9b29d4e7f3fa66151e4276adcb
flynshuePersonal <flynshue@gmail.com>
2021-06-23 08:17:28 -0400

    stub out github api

---
+++ cmd/api.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	nap "github.com/flynshuePersonal/napv2"
+	"github.com/spf13/viper"
+)
+
+var api *nap.API
+
+func GithubAPI() *nap.API {
+	if api == nil {
+		api = nap.NewAPI("https://api.github.com")
+		auth := nap.AuthToken{Token: viper.GetString("token")}
+		api.SetAuth(auth)
+	}
+	return api
+}
