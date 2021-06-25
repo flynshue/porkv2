0259011fdd20e4a47eef5b3e653241e576c1b9c2
Felicia Lyn-Shue <flynshue@gmail.com>
2021-06-25 17:30:56 -0400

    build out fork command

--- cmd/fork.go
+++ cmd/fork.go
@@ -16,10 +16,20 @@ limitations under the License.
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 
+	nap "github.com/flynshuePersonal/napv2"
 	"github.com/spf13/cobra"
 )
+
+type ForkResponse struct {
+	URL string `json:"html_url"`
+}
 
 // forkCmd represents the fork command
 var forkCmd = &cobra.Command{
@@ -42,7 +52,33 @@ to quickly create a Cobra application.`,
 }
 
 func ForkRepo(repo string) error {
+	values := strings.Split(repo, "/")
+	if len(values) != 2 {
+		return fmt.Errorf("must supply repository in owner/project format")
+	}
+	params := map[string]string{"owner": values[0], "project": values[1]}
+	return GithubAPI().Call("fork", params, nil)
+}
+
+func ForkSuccess(resp *http.Response) error {
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	response := &ForkResponse{}
+	if err := json.Unmarshal(b, response); err != nil {
+		return err
+	}
+	fmt.Printf("forked repo to %s\n", response.URL)
 	return nil
+}
+
+func ForkResource() nap.RestResource {
+	router := nap.NewRouter()
+	router.RegisterFunc(202, ForkSuccess)
+	fork := nap.NewResource("POST", "/repos/{{.owner}}/{{.project}}/forks", router)
+	return fork
 }
 
 func init() {
